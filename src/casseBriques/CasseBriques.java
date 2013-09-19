e25050882e6814768c1b90e16692adec77c63e54
sebastienjean <baz.jean@gmail.com>
2013-09-19 21:34:47 +0200

    Added some TODOs
    These TODOs must be processed before doing anything else

--- src/casseBriques/CasseBriques.java
+++ src/casseBriques/CasseBriques.java
@@ -1,9 +1,13 @@
 package casseBriques;
 
+// TODO (fix) comments should be written in english
 /**
  * @author ochiers
+ * // TODO (fix) the class comment is misplaced (must be placed before @author)
  * Classe principale du jeu.
  */
+
+// TODO (fix) the class must be renamed (english) 
 public class CasseBriques
 {
     
