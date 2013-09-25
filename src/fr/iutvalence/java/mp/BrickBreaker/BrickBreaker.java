a61c0a7dd6dc1dcaae8f9e6f2193b4ab34025b25
soulierc <clement.soulier@.iut-valence.fr>
2013-09-25 09:39:24 +0200

    Fixed TODOs : Package is now "fr.iutvalence.java.mp.BrickBreaker" Comments are now in english "CasseBriques" class is now "BrickBreaker" class

---
+++ src/fr/iutvalence/java/mp/BrickBreaker/BrickBreaker.java
@@ -0,0 +1,15 @@
+// TODO (fixed) package name is not as expected (should be fr.iutvalence.java.mp.<projectName>)
+package fr.iutvalence.java.mp.BrickBreaker;
+
+// TODO (fixed) comments should be written in english
+/**
+ * Main class
+ * @author ochiers
+ * // TODO (fixed) the class comment is misplaced (must be placed before @author)
+ */
+
+// TODO (fixed) the class must be renamed (english) 
+public class BrickBreaker
+{
+    
+}
